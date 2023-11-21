e0546dcbfa674fbb269fc5bbbca520ed9b1848ac
yasmi <144814355+cfyascf@users.noreply.github.com>
2023-11-21 10:59:48 -0300

    Update mips.h

--- ASSEMBLY_PROJECT - Copia/mips.h
+++ ASSEMBLY_PROJECT - Copia/mips.h
@@ -28,6 +28,14 @@ void li(MIPSProcessor *cpu, int reg1, int value) {
     cpu->pc += 4; 
 }
 
+void la(MIPSProcessor *cpu, int reg1, char label) {
+    int endereco_label = 1111;
+    cpu->registers[reg1] = endereco_label;
+
+    printf("Endereco da label movido para o registrador!\n");
+    cpu->pc += 4; 
+}
+
 void sw(MIPSProcessor *cpu, int reg1, int reg2, int offset) {
     int registerAddress = offset + reg2;
     cpu->memory[reg1] = cpu->registers[registerAddress];
@@ -41,14 +49,6 @@ void sb(MIPSProcessor *cpu, int reg1, int reg2, int offset) {
     cpu->memory[reg1] = cpu->registers[registerAddress];
 
     printf("Byte resgatado da parte baixa do registrador para a memoria!\n");
-    cpu->pc += 4; 
-}
-
-void la(MIPSProcessor *cpu, int reg1, char label) {
-    int endereco_label = 1111;
-    cpu->registers[reg1] = endereco_label;
-
-    printf("Endereco da label movido para o registrador!\n");
     cpu->pc += 4; 
 }
 
@@ -76,289 +76,298 @@ void mflo(MIPSProcessor *cpu, int reg1) {
 void add(MIPSProcessor *cpu) {
     cpu->registers[aux - 3] = cpu->registers[aux - 2] + cpu->registers[aux - 1];
 
-    printf("Resultado da operacao: %d\n", cpu.registers[aux - 3]);
+    printf("Resultado da operacao: %d\n", cpu->registers[aux - 3]);
     cpu->pc += 4;
 }
 
 void addi(MIPSProcessor *cpu, int value) {
     cpu->registers[aux - 3] = cpu->registers[aux - 2] + value;
 
-    printf("Resultado da operacao: %d\n", cpu.registers[aux - 3]);
+    printf("Resultado da operacao: %d\n", cpu->registers[aux - 3]);
     cpu->pc += 4;
 }
 
 void addu(MIPSProcessor *cpu, int value) {
     cpu->registers[aux - 3] = cpu->registers[aux - 2] + value;
 
-    printf("Resultado da operacao: %d\n", cpu.registers[aux - 3]);
+    printf("Resultado da operacao: %d\n", cpu->registers[aux - 3]);
     cpu->pc += 4;
 }
 
 void sub(MIPSProcessor *cpu) {
     cpu->registers[aux - 3] = cpu->registers[aux - 2] - cpu->registers[aux - 1];
 
-    printf("Resultado da operacao: %d\n", cpu.registers[aux - 3]);
+    printf("Resultado da operacao: %d\n", cpu->registers[aux - 3]);
     cpu->pc += 4;
 }
 
 void subu(MIPSProcessor *cpu) {
     cpu->registers[aux - 3] = cpu->registers[aux - 2] - cpu->registers[aux - 1];
 
-    printf("Resultado da operacao: %d\n", cpu.registers[aux - 3]);
+    printf("Resultado da operacao: %d\n", cpu->registers[aux - 3]);
     cpu->pc += 4;
 }
 
 void mul(MIPSProcessor *cpu) {
     cpu->registers[aux - 3] = cpu->registers[aux - 2] * cpu->registers[aux - 1];
 
-    printf("Resultado da operacao: %d\n", cpu.registers[aux - 3]);
+    printf("Resultado da operacao: %d\n", cpu->registers[aux - 3]);
     cpu->pc += 4;
 }
 
 void mulo(MIPSProcessor *cpu) {
     cpu->registers[aux - 3] = cpu->registers[aux - 2] * cpu->registers[aux - 1];
 
-    printf("Resultado da operacao: %d\n", cpu.registers[aux - 3]);
+    printf("Resultado da operacao: %d\n", cpu->registers[aux - 3]);
     cpu->pc += 4;
 }
 
 void mulou(MIPSProcessor *cpu) {
     cpu->registers[aux - 3] = cpu->registers[aux - 2] * cpu->registers[aux - 1];
 
-    printf("Resultado da operacao: %d\n", cpu.registers[aux - 3]);
+    printf("Resultado da operacao: %d\n", cpu->registers[aux - 3]);
     cpu->pc += 4;
 }
 
 void mult(MIPSProcessor *cpu) {
     cpu->registers[lo] = cpu->registers[aux - 2] * cpu->registers[aux - 1];
 
-    printf("Resultado da operacao: %d\n", cpu.registers[aux - 3]);
+    printf("Resultado da operacao: %d\n", cpu->registers[aux - 3]);
     cpu->pc += 4;
 }
 
 void multu(MIPSProcessor *cpu) {
     cpu->registers[lo] = cpu->registers[aux - 2] * cpu->registers[aux - 1];
 
-    printf("Resultado da operacao: %d\n", cpu.registers[aux - 3]);
+    printf("Resultado da operacao: %d\n", cpu->registers[aux - 3]);
     cpu->pc += 4;
 }
 
 void div(MIPSProcessor *cpu) {
     cpu->registers[aux - 3] = cpu->registers[aux - 2] / cpu->registers[aux - 1];
 
-    printf("Resultado da operacao: %d\n", cpu.registers[aux - 3]);
+    printf("Resultado da operacao: %d\n", cpu->registers[aux - 3]);
     cpu->pc += 4;
 }
 
 void divu(MIPSProcessor *cpu) {
     cpu->registers[aux - 3] = cpu->registers[aux - 2] / cpu->registers[aux - 1];
 
-    printf("Resultado da operacao: %d\n", cpu.registers[aux - 3]);
+    printf("Resultado da operacao: %d\n", cpu->registers[aux - 3]);
     cpu->pc += 4;
 }
 
 void rem(MIPSProcessor *cpu) {
     cpu->registers[aux - 3] = cpu->registers[aux - 2] % cpu->registers[aux - 1];
 
-    printf("Resultado da operacao: %d\n", cpu.registers[aux - 3]);
+    printf("Resultado da operacao: %d\n", cpu->registers[aux - 3]);
     cpu->pc += 4;
 }
 
 void remu(MIPSProcessor *cpu) {
     cpu->registers[aux - 3] = cpu->registers[aux - 2] % cpu->registers[aux - 1];
 
-    printf("Resultado da operacao: %d\n", cpu.registers[aux - 3]);
+    printf("Resultado da operacao: %d\n", cpu->registers[aux - 3]);
     cpu->pc += 4;
 }
 
 void andf(MIPSProcessor *cpu) {
     cpu->registers[aux - 3] = cpu->registers[aux - 2] & cpu->registers[aux - 1];
 
-    printf("Resultado da operacao: %d\n", cpu.registers[aux - 3]);
+    printf("Resultado da operacao: %d\n", cpu->registers[aux - 3]);
     cpu->pc += 4;
 }
 
 void andi(MIPSProcessor *cpu, int value) {
     cpu->registers[aux - 3] = cpu->registers[aux - 2] & value;
 
-    printf("Resultado da operacao: %d\n", cpu.registers[aux - 3]);
+    printf("Resultado da operacao: %d\n", cpu->registers[aux - 3]);
     cpu->pc += 4;
 }
 
 void neg(MIPSProcessor *cpu) {
     cpu->registers[aux - 3] = 0 - cpu->registers[aux - 2];
 
-    printf("Resultado da operacao: %d\n", cpu.registers[aux - 3]);
+    printf("Resultado da operacao: %d\n", cpu->registers[aux - 3]);
     cpu->pc += 4;
 }
 
 void negu(MIPSProcessor *cpu) {
     cpu->registers[aux - 3] = 0 - cpu->registers[aux - 2];
 
-    printf("Resultado da operacao: %d\n", cpu.registers[aux - 3]);
+    printf("Resultado da operacao: %d\n", cpu->registers[aux - 3]);
     cpu->pc += 4;
 }
 
 void nor(MIPSProcessor *cpu) {
     cpu->registers[aux - 3] = ~(cpu->registers[aux - 2] | cpu->registers[aux - 1]);
 
-    printf("Resultado da operacao: %d\n", cpu.registers[aux - 3]);
+    printf("Resultado da operacao: %d\n", cpu->registers[aux - 3]);
     cpu->pc += 4;
 }
 
 void notf(MIPSProcessor *cpu) {
     cpu->registers[aux - 3] = ~cpu->registers[aux - 2];
 
-    printf("Resultado da operacao: %d\n", cpu.registers[aux - 3]);
+    printf("Resultado da operacao: %d\n", cpu->registers[aux - 3]);
     cpu->pc += 4;
 }
 
 void orf(MIPSProcessor *cpu) {
     cpu->registers[aux - 3] = cpu->registers[aux - 2] | cpu->registers[aux - 1];
 
-    printf("Resultado da operacao: %d\n", cpu.registers[aux - 3]);
+    printf("Resultado da operacao: %d\n", cpu->registers[aux - 3]);
     cpu->pc += 4;
 }
 
 void ori(MIPSProcessor *cpu, int value) {
     cpu->registers[aux - 3] = cpu->registers[aux - 2] | value;
 
-    printf("Resultado da operacao: %d\n", cpu.registers[aux - 3]);
+    printf("Resultado da operacao: %d\n", cpu->registers[aux - 3]);
     cpu->pc += 4;
 }
 
 void rol(MIPSProcessor *cpu, int value) {
     cpu->registers[aux - 3] = cpu->registers[aux - 2] << value | cpu->registers[aux - 2] >> (32 - value);
 
-    printf("Resultado da operacao: %d\n", cpu.registers[aux - 3]);
+    printf("Resultado da operacao: %d\n", cpu->registers[aux - 3]);
     cpu->pc += 4;
 }
 
 void ror(MIPSProcessor *cpu, int value) {
     cpu->registers[aux - 3] = cpu->registers[aux - 2] >> value | cpu->registers[aux - 2] << (32 - value);
 
-    printf("Resultado da operacao: %d\n", cpu.registers[aux - 3]);
+    printf("Resultado da operacao: %d\n", cpu->registers[aux - 3]);
     cpu->pc += 4;
 }
 
 void sll(MIPSProcessor *cpu, int value) {
     cpu->registers[aux - 3] = cpu->registers[aux - 2] << value ;
 
-    printf("Resultado da operacao: %d\n", cpu.registers[aux - 3]);
+    printf("Resultado da operacao: %d\n", cpu->registers[aux - 3]);
     cpu->pc += 4;
 }
 
 void sla(MIPSProcessor *cpu, int value) {
     cpu->registers[aux - 3] = cpu->registers[aux - 2] << value ;
 
-    printf("Resultado da operacao: %d\n", cpu.registers[aux - 3]);
+    printf("Resultado da operacao: %d\n", cpu->registers[aux - 3]);
     cpu->pc += 4;
 }
 
 void srl(MIPSProcessor *cpu, int value) {
     cpu->registers[aux - 3] = cpu->registers[aux - 2] >> value ;
 
-    printf("Resultado da operacao: %d\n", cpu.registers[aux - 3]);
+    printf("Resultado da operacao: %d\n", cpu->registers[aux - 3]);
     cpu->pc += 4;
 }
 
 void sra(MIPSProcessor *cpu, int value) {
     cpu->registers[aux - 3] = cpu->registers[aux - 2] >> value ;
 
-    printf("Resultado da operacao: %d\n", cpu.registers[aux - 3]);
+    printf("Resultado da operacao: %d\n", cpu->registers[aux - 3]);
     cpu->pc += 4;
 }
 
 void xorf(MIPSProcessor *cpu) {
     cpu->registers[aux - 3] = cpu->registers[aux - 2] ^ cpu->registers[aux - 1];
 
-    printf("Resultado da operacao: %d\n", cpu.registers[aux - 3]);
+    printf("Resultado da operacao: %d\n", cpu->registers[aux - 3]);
     cpu->pc += 4;
 }
 
 void xori(MIPSProcessor *cpu, int value) {
     cpu->registers[aux - 3] = cpu->registers[aux - 2] ^ value;
 
-    printf("Resultado da operacao: %d\n", cpu.registers[aux - 3]);
+    printf("Resultado da operacao: %d\n", cpu->registers[aux - 3]);
     cpu->pc += 4;
 }
  
 void b(MIPSProcessor *cpu, char label) {
-    pc = endereco_label;
-    printf("Valor de pc atualizado: %d\n", pc);
+    cpu->pc = endereco_label;
+    printf("Valor de pc atualizado: %d\n", cpu->pc);
 }
 
 void j(MIPSProcessor *cpu, char label) {
-    pc = endereco_label;
-    printf("Valor de pc atualizado: %d\n", pc);
+    cpu->pc = endereco_label;
+    printf("Valor de pc atualizado: %d\n", cpu->pc);
 }
 
 void jr(MIPSProcessor *cpu, int reg1) {
     int endereco_reg1 = 3;
 
-    pc = endereco_reg1;
-    printf("Valor de pc atualizado: %d\n", pc);
+    cpu->pc = endereco_reg1;
+    printf("Valor de pc atualizado: %d\n", cpu->pc);
 }
 
 void beq(MIPSProcessor *cpu, char label, int reg1, int reg2) {
     if(reg1 == reg2){
-        pc = label;
+        cpu->pc = label;
     }
 
-    printf("Valor de pc atualizado: %d\n", pc);
+    printf("Valor de pc atualizado: %d\n", cpu->pc);
 }
 
 void blt(MIPSProcessor *cpu, char label, int reg1, int reg2) {
     if(reg1 < reg2){
-        pc = label;
+        cpu->pc = label;
     }
     
-    printf("Valor de pc atualizado: %d\n", pc);
+    printf("Valor de pc atualizado: %d\n", cpu->pc);
 }
 
 void ble(MIPSProcessor *cpu, char label, int reg1, int reg2) {
     if(reg1 <= reg2){
-        pc = label;
+        cpu->pc = label;
     }
     
-    printf("Valor de pc atualizado: %d\n", pc);
+    printf("Valor de pc atualizado: %d\n", cpu->pc);
 }
 
 void bgt(MIPSProcessor *cpu, char label, int reg1, int reg2) {
     if(reg1 > reg2){
-        pc = label;
+        cpu->pc = label;
     }
     
-    printf("Valor de pc atualizado: %d\n", pc);
+    printf("Valor de pc atualizado: %d\n", cpu->pc);
 }
 
 void bge(MIPSProcessor *cpu, char label, int reg1, int reg2) {
     if(reg1 >= reg2){
-        pc = label;
+        cpu->pc = label;
     }
     
-    printf("Valor de pc atualizado: %d\n", pc);
+    printf("Valor de pc atualizado: %d\n", cpu->pc);
 }
 
 void bne(MIPSProcessor *cpu, char label, int reg1, int reg2) {
     if(reg1 != reg2){
-        pc = label;
+        cpu->pc = label;
     }
     
-    printf("Valor de pc atualizado: %d\n", pc);
+    printf("Valor de pc atualizado: %d\n", cpu->pc);
 }
 
 void jal(MIPSProcessor *cpu, char label) {
-    pc = label;
-    cpu->registers[ra] = pc;
+    cpu->pc = label;
+    cpu->registers[ra] = cpu->pc;
     
-    printf("Valor de pc atualizado: %d\n", pc);
+    printf("Valor de pc atualizado: %d\n", cpu->pc);
 }
 
 void jr_ra(MIPSProcessor *cpu, char label) {
-    pc = cpu->registers[ra];
+    cpu->pc = cpu->registers[ra];
     
-    printf("Valor de pc atualizado: %d\n", pc);
+    printf("Valor de pc atualizado: %d\n", cpu->pc);
+}
+
+void execute(int opcode, MIPSProcessor *cpu, int reg1, int reg2, int offset) {
+    if (opcode == 0x20)
+        add(cpu);
+    else if (opcode == 0x22)
+        sub(cpu);
+    else if(opcode == 0x23)
+        lw(cpu, reg1, reg2, offset);
 }
 
 #endif
