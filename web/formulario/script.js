9c7d32d4b7f1128254c961a3748d06d19b0a4df3
yasmi <144814355+cfyascf@users.noreply.github.com>
2024-03-14 12:56:04 -0300

    Delete web/formulario directory

--- web/formulario/script.js
+++
@@ -1,5 +0,0 @@
-const clearButton = document.getElementById("clearButton")
-clearButton.addEventListener("click", () => {
-    const inputs = Array.from(document.querySelectorAll("input"))
-    inputs.forEach((input) => input.value = "")
-})
